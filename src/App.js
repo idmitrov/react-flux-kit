6897389469056ebab1380556be592d445c8d32dd
idmitrov <mailto.dimitrov@gmail.com>
2017-07-15 21:00:33 +0300

    move drawer list as state to Layout instead of App

--- src/App.js
+++ src/App.js
@@ -7,23 +7,10 @@ import AppLayout from './components/shared/AppLayout';
 import AppRoutes from './components/shared/AppRoutes';
 
 class App extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            name: 'My App',
-            drawer: [
-                { name: 'Home', href: '/', title: 'Home' },
-                { name: 'About', href: '/about', title: 'About' },
-                { name: 'Test', href: '/test', title: 'Test' }
-            ]
-        };
-    }
-
     render() {
         return (
             <MuiThemeProvider>
-                <AppLayout heading={this.state.name} drawer={this.state.drawer}>
+                <AppLayout heading="My App">
                     <AppRoutes />
                 </AppLayout>
             </MuiThemeProvider>
