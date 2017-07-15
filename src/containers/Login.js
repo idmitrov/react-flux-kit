79a34c978b7465a65a115541ac84ce84b5600357
idmitrov <mailto.dimitrov@gmail.com>
2017-07-15 21:49:04 +0300

    Implement Login/Register

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,12 +1,84 @@
 import React, { Component } from 'react';
 
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+
+import requester from '../utils/requester';
+
 class Login extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            email: null,
+            password: null
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
     render() {
-        return(
+        return (
             <section>
-                <h2>Login</h2>
+                <div className="row">
+                    <div className="col-md-12">
+                        <h2>Login</h2>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <TextField
+                            name="email"
+                            label="Email"
+                            type="email"
+                            required
+                            marginForm
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <TextField
+                            name="password"
+                            label="Password"
+                            type="password"
+                            required
+                            marginForm
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <Button
+                            raised
+                            color="contrast"
+                            onClick={this.handleSubmit}>
+                            Login
+                                </Button>
+                    </div>
+                </div>
             </section>
         );
+    }
+
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value
+        });
+    }
+
+    handleSubmit() {
+        // TODO: Call userAction instaad
+        requester.post('/auth/login', this.state)   
+            .then(response => {
+                console.log(response);
+            });
     }
 }
 
--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+
 import requester from '../utils/requester';
 
 class Register extends Component {
@@ -6,24 +10,88 @@ class Register extends Component {
         super();
 
         this.state = {
-            user: {
-                name: null,
-                email: null,
-                password: null
-            }
+            name: null,
+            email: null,
+            password: null
         };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     render() {
-        requester.post('/auth/signup', this.state.user)
+        return (
+            <section>
+                <div className="row">
+                    <div className="col-md-12">
+                        <h2>Register</h2>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <TextField
+                            name="email"
+                            label="Email"
+                            type="email"
+                            required
+                            marginForm
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <TextField
+                            name="name"
+                            label="Name"
+                            required
+                            marginForm
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <TextField
+                            name="password"
+                            label="Password"
+                            type="password"
+                            required
+                            marginForm
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <Button
+                            raised
+                            color="contrast"
+                            onClick={this.handleSubmit}>
+                            Register
+                        </Button>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value
+        });
+    }
+
+    handleSubmit() {
+        // TODO: Call userAction instaad
+        requester.post('/auth/signup', this.state)
             .then(response => {
                 console.log(response);
             });
-        return (
-            <section>
-                <h2>Register</h2>
-            </section>
-        );
     }
 }
 
