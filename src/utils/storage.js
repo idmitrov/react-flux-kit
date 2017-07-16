0b82b1261caa8a9a6f17723e3759f9d5cad363d8
idmitrov <mailto.dimitrov@gmail.com>
2017-07-16 11:11:56 +0300

    Implement storage

---
+++ src/utils/storage.js
@@ -0,0 +1,50 @@
+const storage = {
+    /**
+     * @name get
+     * @param {String} key
+     * @desc Get storage item
+     * @return {String} item
+     */
+    get: (key) => {
+        return localStorage.getItem(key);
+    },
+    /**
+     * @name add
+     * @param {String} key
+     * @param {String} value
+     * @desc Add storage item
+     */
+    add: (key, value) => {
+        if (this.get(key)) {
+            console.warn(`Storage already containst an item called: ${key}`);
+        } else {
+            localStorage.setItem(key, value);
+        }
+    },
+     /**
+     * @name update
+     * @param {String} key
+     * @param {String} value
+     * @desc Update an existing storage item
+     */
+    update: (key, value) => {
+        localStorage.setItem(key, value);
+    },
+    /**
+     * @name delete
+     * @param {String} key
+     * @desc Delete storage item
+     */
+    delete: (key) => {
+        localStorage.removeItem(key);
+    },
+    /**
+     * @name clear
+     * @desc Clear all storage items
+     */
+    clear: () => {
+        localStorage.clear();
+    }
+};
+
+export default storage;
